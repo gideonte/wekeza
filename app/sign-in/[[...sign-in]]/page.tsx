f5114a2f9a0f0fec021c4812484179fdabe2abfd
Gideon <gmsambwa@gmail.com>
2025-06-09 13:54:42 -0600

    Disable sign-up

--- app/sign-in/[[...sign-in]]/page.tsx
+++ app/sign-in/[[...sign-in]]/page.tsx
@@ -5,7 +5,13 @@ import { SignIn } from "@clerk/nextjs";
 export default function Page() {
   return (
     <div className="flex min-h-screen items-center justify-center p-4">
-      <SignIn />
+      <SignIn
+        appearance={{
+          elements: {
+            footer: { display: "none" }, // Hides "Don't have an account? Sign up"
+          },
+        }}
+      />
     </div>
   );
 }
